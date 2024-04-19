b878dcaa59d894708b8b04f64b2f9ee149db1a5e
Hbaril18 <barilhenrick@gmail.com>
2024-04-19 14:50:26 -0400

    refactor : Split du script pour etre plus cohérent (FSM)

--- Dev/GeeksLegacy/Assets/Scripts/Henrick/FSM.cs
+++ Dev/GeeksLegacy/Assets/Scripts/Henrick/FSM.cs
@@ -1,92 +1,54 @@
+using System;
 using System.Collections.Generic;
-using NUnit.Framework;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
+using System.Text;
+using Unity;
 
-/*
-Le code a été tirer de ce site : https://faramira.com/generic-finite-state-machine-using-csharp/
-Aussi aidé à l'aide de : https://faramira.com/enemy-behaviour-with-finite-state-machine-using-csharp-delegates-in-unity/
- */
-
-//Namespaces are used in C# to organize and provide a level of separation of codes. (Google definition)
-namespace FSM
+namespace Patterns
 {
-    //<T> symbol for a generic type parameter. (Google definition)
-    public class State<T>
+    public class FiniteStateMachine<T>
     {
-        //if we want to pass a function as a parameter? How does C# handles
-        //the callback functions or event handler? The answer is - delegate. (Google definition)
-        public delegate void DelegateNoArg();
-        public DelegateNoArg OnEnter;
-        public DelegateNoArg OnExit;
-        public DelegateNoArg OnUpdate;
-        public DelegateNoArg OnFixedUpdate;
+        // A Finite State Machine
+        //    - consists of a set of states,
+        //    - and at any given time, an FSM can exist in only one 
+        //      State out of these possible set of states.
 
-        //Collection représentant un set de state
+        // A dictionary to represent the a set of states.
         protected Dictionary<T, State<T>> mStates;
 
-        // Le state actuel
+        // The current state.
         protected State<T> mCurrentState;
 
-        // Nom du state
-        public string Name { get; set; } //getter et setter
-
-        // Id du state
-        public T ID { get; private set; } //getter et setter
-
-        //constructor
-        public State(T id)
-        {
-            ID = id;
-        }
-
-        //constructor
-        public State(T id, string name) : this(id)
-        {
-            Name = name;
-        }
-
-        //default constructor
-        public void FiniteStateMachine()
+        public FiniteStateMachine()
         {
             mStates = new Dictionary<T, State<T>>();
         }
 
-        //add a new state
         public void Add(State<T> state)
         {
             mStates.Add(state.ID, state);
         }
+
         public void Add(T stateID, State<T> state)
         {
             mStates.Add(stateID, state);
         }
 
-        //returns a State based on the key
-        public State<T> GetState(T stateID) //will return null if a State of the same key has not been added previously to the FSM
+        public State<T> GetState(T stateID)
         {
             if (mStates.ContainsKey(stateID))
                 return mStates[stateID];
             return null;
         }
 
-        //Virtual methods are method that can be overridden in a derived class (Google definition)
-        virtual public void Enter()
+        public void SetCurrentState(T stateID)
         {
-            OnEnter?.Invoke();
+            State<T> state = mStates[stateID];
+            SetCurrentState(state);
         }
-        virtual public void Exit()
+
+        public State<T> GetCurrentState()
         {
-            OnExit?.Invoke();
-        }
-        virtual public void Update()
-        {
-            OnUpdate?.Invoke();
-        }
-        virtual public void FixedUpdate()
-        {
-            OnFixedUpdate?.Invoke();
+            return mCurrentState;
         }
 
         public void SetCurrentState(State<T> state)
@@ -95,51 +57,34 @@ namespace FSM
             {
                 return;
             }
+
             if (mCurrentState != null)
             {
                 mCurrentState.Exit();
-                mCurrentState = state;
+            }
+
+            mCurrentState = state;
+
+            if (mCurrentState != null)
+            {
                 mCurrentState.Enter();
             }
         }
 
-        public State(T id,
-            DelegateNoArg onEnter,
-            DelegateNoArg onExit = null,
-            DelegateNoArg onUpdate = null,
-            DelegateNoArg onFixedUpdate = null) : this(id)
+        public void Update()
         {
-            OnEnter = onEnter;
-            OnExit = onExit;
-            OnUpdate = onUpdate;
-            OnFixedUpdate = onFixedUpdate;
+            if (mCurrentState != null)
+            {
+                mCurrentState.Update();
+            }
         }
-        public State(T id,
-            string name,
-            DelegateNoArg onEnter,
-            DelegateNoArg onExit = null,
-            DelegateNoArg onUpdate = null,
-            DelegateNoArg onFixedUpdate = null) : this(id, name)
+
+        public void FixedUpdate()
         {
-            OnEnter = onEnter;
-            OnExit = onExit;
-            OnUpdate = onUpdate;
-            OnFixedUpdate = onFixedUpdate;
+            if (mCurrentState != null)
+            {
+                mCurrentState.FixedUpdate();
+            }
         }
     }
 }
-
-//EX D'UTILISATION : 
-
-        /*mFsm = new FSM();
-            mFsm.Add((int)StateTypes.IDLE, new NPCState(mFsm, StateTypes.IDLE, this));
-            mFsm.Add((int)StateTypes.CHASE, new NPCState(mFsm, StateTypes.CHASE, this));
-            mFsm.Add((int)StateTypes.ATTACK, new NPCState(mFsm, StateTypes.ATTACK, this));
-            mFsm.Add((int)StateTypes.DAMAGE, new NPCState(mFsm, StateTypes.DAMAGE, this));
-            mFsm.Add((int)StateTypes.DIE, new NPCState(mFsm, StateTypes.DIE, this));
-            Init_IdleState();
-            Init_AttackState();
-            Init_DieState();
-            Init_DamageState();
-            Init_ChaseState();
-            mFsm.SetCurrentState(mFsm.GetState((int)StateTypes.IDLE));*/
