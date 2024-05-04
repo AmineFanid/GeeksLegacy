6bb2b46a8f7db7290940697f76013e87e5461311
HBaril18 <barilhenrick@gmail.com>
2024-05-03 20:13:55 -0400

    Refactor : Réglage de tous les bugs + changement pour le respectdes conventions

--- Dev/GeeksLegacy/Assets/Scripts/Amine/ControlCharacters.cs
+++ Dev/GeeksLegacy/Assets/Scripts/Amine/ControlCharacters.cs
@@ -1,66 +1,58 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Rigidbody2D))]
 
 public class ControlCharacters : MonoBehaviour
 {
-
-    private Animator _Animator;
-    private Rigidbody2D _Rigidbody;
-
-    private float ControleX;
     [Header("Character Settings")]
+    [Header("Jump Settings")]
     [SerializeField] private float _JumpHeight = 2.0f;
-    [SerializeField] private float _Speed = 5.0f;
-    [SerializeField] private float _JumpForce = 2.0f;
+    [SerializeField] private int _MaxJump = 2;
     [SerializeField] private float _GravityScale = 1.0f;
     [SerializeField] private float _FallingGravityScale = 10.0f;
+    [Header("Speed Settings")]
+    [SerializeField] private float _Speed = 5.0f;
     [SerializeField] private float _MaxSpeed = 7.0f;
+    [SerializeField] private float _MaxYSpeed = 20.0f;
+    [Header("Player Interactions")]
     [SerializeField] private float _PlayerLife = 100.0f;
     [SerializeField] private float _KnockBack = 4.0f;
-    [SerializeField] public float _KBCounter = 0.0f;
-    [SerializeField] public float _KBTotalTime = 1.0f;
-    [SerializeField] public float _InvCounter = 0.0f;
-    [SerializeField] public float _InvTotalTime = 1.0f;
-    [SerializeField] public float _MaxYSpeed = 20.0f;
+    [SerializeField] public float kBCounter = 0.0f;
+    [SerializeField] public float kBTotalTime = 1.0f;
 
-    private int _MaxJump = 2;
-    private int _NumJump = 0;
-    private Vector2 movement;
     public IEnumerator invincibility;
-    public float _CurrentLife;
     public Player player;
     public CharacterInventory inventory;
     public bool knockRight;
-
+    private int _NumJump = 0;
+    private Vector2 _Movement;
     private bool _isGrounded;
-
+    private Animator _Animator;
+    private Rigidbody2D _Rigidbody;
+    private float _ControleX;
+    private int playerLayer;
+    private int ennemiesLayer;
 
     public void Start() {
         _Animator = GetComponent<Animator>();
         _Rigidbody = GetComponent<Rigidbody2D>();
         inventory = new CharacterInventory();
         player = new Player(_PlayerLife, inventory);
+        playerLayer = LayerMask.NameToLayer("Player");
+        ennemiesLayer = LayerMask.NameToLayer("Ennemies");
     }
 
     public void Update()
     {
+        //Mouvement horizontal
+        _ControleX = Input.GetAxis("Horizontal");  
+        _Movement = new Vector2(_ControleX, 0f);
+        _Rigidbody.AddForce(_Movement * _Speed);
 
-        ControleX = Input.GetAxis("Horizontal");
-
-        // Move horizontally    
-        movement = new Vector2(ControleX, 0f);
-        _Rigidbody.AddForce(movement * _Speed);
-
-        // Saute
+        //Mouvement vertical
         if (Input.GetButtonDown("Jump"))
         {
             if(_NumJump < _MaxJump)
@@ -70,59 +62,95 @@ public class ControlCharacters : MonoBehaviour
             }
         }
 
-        Debug.Log(_Rigidbody.velocity);
-        Debug.Log(_GravityScale);
-        Debug.Log(_Rigidbody.gravityScale);
+        //Vérification du maximum de vitesse
+        MaxingSpeed();
     }
 
     public void FixedUpdate()
     {
-        if (_Rigidbody.velocity.y >= -0.001f)
-            _Rigidbody.gravityScale = _GravityScale;
-        else { 
-            _Rigidbody.gravityScale += _FallingGravityScale;
-        }
+        //Vérification de vitesse
+        GravitiCheck();
 
-        int playerLayer = LayerMask.NameToLayer("Player");
-        int ennemiesLayer = LayerMask.NameToLayer("Ennemies");
-        if (_KBCounter <= 0)
+        //Vérification de KnockBack
+        KnockBackVerif();
+    }
+
+    public void KnockBack()
+    {
+        if (knockRight)
+            _Rigidbody.velocity = new Vector2(-_KnockBack, _KnockBack / 2);
+        else
+            _Rigidbody.velocity = new Vector2(_KnockBack, _KnockBack / 2);
+    }
+
+    public void KnockBackVerif()
+    {
+        if (kBCounter <= 0)
         {
             _Animator.SetBool("GetHit", false);
             Physics2D.IgnoreLayerCollision(playerLayer, ennemiesLayer, false);
+
             int groundLayerMask = LayerMask.GetMask("Ground");
             _isGrounded = Physics2D.Raycast(transform.position, Vector2.down, 0.9f, groundLayerMask);
 
             if ((_NumJump >= _MaxJump) && _isGrounded)
                 _NumJump = 0;
 
-            float vitesse = _Rigidbody.velocityX;
-            bool vitesseBouge = vitesse > 0.1f || vitesse < -0.1f;
-            _Animator.SetBool("IsMoving", vitesseBouge);
-            if (vitesseBouge)
-            {
-                //Active ses animations en fonction du mouvement
-                Vector2 directionAssainie = ForceAnimationVirtualJoystick.ForceDirectionAxe(movement);
-                _Animator.SetFloat("MouvementX", directionAssainie.x);
-                _Animator.SetFloat("MouvementY", directionAssainie.y);
-            }
-
-            //Vérification pour limité la vitesse en x
-            if (_Rigidbody.velocity.x >= _MaxSpeed)
-                _Rigidbody.velocity = new Vector2(_MaxSpeed, _Rigidbody.velocity.y);
-            if (_Rigidbody.velocity.x <= _MaxSpeed * -1)
-                _Rigidbody.velocity = new Vector2(_MaxSpeed * -1, _Rigidbody.velocity.y);
+            MouvementAnimation();
         }
-        else {
+        else
+        {
             _Animator.SetBool("GetHit", true);
             Physics2D.IgnoreLayerCollision(playerLayer, ennemiesLayer, true);
-            Debug.Log(knockRight);
-            if (knockRight)
-                _Rigidbody.velocity = new Vector2(-_KnockBack, _KnockBack/2);
-            else
-                _Rigidbody.velocity = new Vector2(_KnockBack, _KnockBack / 2);
 
-            _KBCounter -= Time.deltaTime;
+            KnockBack();
+            kBCounter -= Time.deltaTime;
         }
+    }
+
+    public void MouvementAnimation()
+    {
+        float vitesse = _Rigidbody.velocityX;
+        bool vitesseBouge = vitesse > 0.1f || vitesse < -0.1f;
+        _Animator.SetBool("IsMoving", vitesseBouge);
+        if (vitesseBouge)
+        {
+            //Active ses animations en fonction du mouvement
+            Vector2 directionAssainie = ForceAnimationVirtualJoystick.ForceDirectionAxe(_Movement);
+            _Animator.SetFloat("MouvementX", directionAssainie.x);
+            _Animator.SetFloat("MouvementY", directionAssainie.y);
+        }
+    }
+
+    public void MaxingSpeed()
+    {
+        //Vérification pour limité la vitesse en x positif
+        if (_Rigidbody.velocity.x >= _MaxSpeed)
+        {
+            _Rigidbody.velocity = new Vector2(_MaxSpeed, _Rigidbody.velocity.y);
+            //Vérification pour limité la vitesse en y négatif
+            if (_Rigidbody.velocity.y <= _MaxYSpeed * -1)
+                _Rigidbody.velocity = new Vector2(_MaxSpeed, _MaxYSpeed * -1);
+        }
+        //Vérification pour limité la vitesse en x négatif
+        if (_Rigidbody.velocity.x <= _MaxSpeed * -1)
+        {
+            _Rigidbody.velocity = new Vector2(_MaxSpeed * -1, _Rigidbody.velocity.y);
+            //Vérification pour limité la vitesse en y négatif
+            if (_Rigidbody.velocity.y <= _MaxYSpeed * -1)
+                _Rigidbody.velocity = new Vector2(_MaxSpeed * -1, _MaxYSpeed * -1);
+        }
+        //Vérification pour limité la vitesse en y négatif
+        if (_Rigidbody.velocity.y <= _MaxYSpeed * -1)
+            _Rigidbody.velocity = new Vector2(_Rigidbody.velocity.x, _MaxYSpeed * -1);
+    }
+
+    public void GravitiCheck()
+    {
+        if (_Rigidbody.velocity.y >= -0.001f)
+            _Rigidbody.gravityScale = _GravityScale;
+        else
+            _Rigidbody.gravityScale += _FallingGravityScale;
     }
 
     public Player findPlayerObject()
@@ -142,11 +170,14 @@ public class Player
         this.inventory = inventory;
     }
 
-    public float GetLifePoint() {
+    public float GetLifePoint()
+    {
         return this.lifePoint;
     }
 
-    public void TakeDamage(float damage) {       
+    public void TakeDamage(float damage)
+    {
         this.lifePoint -= damage;
+        EventManager.TriggerEvent(EventManager.PossibleEvent.eVieJoueurChange, this.lifePoint);
     }
 }
