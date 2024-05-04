1ff210ab87d610deeb66cf2fd0d05d70ef67d1d0
AmineFanid <fan.aminee@gmail.com>
2024-05-03 21:06:11 -0400

    Feat : Avancement dans les scripts pour la gestion de la barre de vie et pour l'observer

--- Dev/GeeksLegacy/Assets/Scripts/Amine/HealthBar.cs
+++ Dev/GeeksLegacy/Assets/Scripts/Amine/HealthBar.cs
@@ -14,9 +14,11 @@ public class HealthBar : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _Animator = GetComponent<Animator>();
         _EcouteurBruit += updateHealthBar;
         _PLayerObject = GameObject.FindGameObjectWithTag("Player");
         _PlayerControl = _PLayerObject.GetComponent<ControlCharacters>();
+        playersLife = _PlayerControl.findPlayerObject().GetLifePoint(); // Pour initialiser la variable playersLife avec la vie actuelle du joueur
         EventManager.StartListening(EventManager.PossibleEvent.eVieJoueurChange, _EcouteurBruit); // Subscribe to ePlayerLifeChanged event
 
 
@@ -25,12 +27,13 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //print("UPDATE'S playersLife" + playersLife);
+        _Animator.SetFloat("CurrentHP", playersLife); //Pour mettre a jour le sprite (animation) de la barre de vie
     }
 
-    public void updateHealthBar(object newLife)
+    public void updateHealthBar(object newLife)//Pour mettre a jour playersLife
     {
-        //ICI ON VA UPDATE LES ANIMATIONS DE LA HEALTHBAR
-        print("updating healthbar");
+        playersLife = (float)newLife;       
+        //print("new playersLife" + playersLife);
     }
 }
